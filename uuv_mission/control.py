13e08b4b932f034dad8ba16f2e1832f0e3a38627
Tom Owen <towen365@gmail.com>
2025-10-15 12:13:33 +0000

    Control class updated with proportional controller

--- uuv_mission/control.py
+++ uuv_mission/control.py
@@ -1,6 +1,12 @@
 class Control:
     def __init__(self):
-        self.Kp = 0
-        self.Kd = 0
-    def get_action(self,observation,reference):
-        return 
+        self.Kp = 0.15
+        self.Kd = 0.6
+        self.prev = 0
+        self.error = 0
+        self.action = 0
+    def get_action(self, observation, reference):
+        self.error = reference - observation
+        self.action = self.Kp * self.error + self.Kd * (self.error - self.prev)
+        self.prev = self.error
+        return self.action
